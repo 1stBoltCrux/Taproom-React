90aa3b6247d8c08ed598a52c363edde127e35d6b
Ted Leary <markeleary@gmail.com>
2018-06-29 14:24:44 -0700

    add modules to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import './header.css';
-import logo from './../assets/images/logo.png'
+
+import logo from './../assets/images/logo.png';
 import {Link} from 'react-router-dom';
+import styles from './header.css'
 
 export default function Header(){
   return(
-    <div className='header-wrapper'>
-      <div className='header-logo'>
+    <div className={styles.headerWrapper}>
+      <div className={styles.headerLogo}>
         <img src={logo}/>
       </div>
-      <div className='header-links'>
+      <div className={styles.headerLinks}>
         <ul>
           <Link to='/keglist'><li>View Our Selection</li></Link>
           <Link to='/addkeg'><li>Add a New Keg</li></Link>
