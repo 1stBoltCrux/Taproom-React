933e14737482d170b3a5a9d1f39140a10f7b7fa0
Ted Leary <markeleary@gmail.com>
2018-07-06 07:57:16 -0700

    remove state from kegslist, move to app

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -2,67 +2,10 @@ import React from 'react';
 import styles from './keglist.css';
 import Keg from './Keg.jsx';
 
-export default class KegList extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      masterKegList: [
-
-        {
-          name: 'Ruby Zozzle',
-          brewer: 'Hi-Wheel',
-          description: 'Sparkling Wine & Grapefruit',
-          abv: '6.8%',
-          price: '7',
-          remaining: '20'
-        },
-        {
-          name: 'Tart N Juicy',
-          brewer: 'Epic',
-          description: 'Sour IPA',
-          abv: '4.5%',
-          price: '6',
-          remaining: '60'
-        },
-        {
-          name: 'Hamm\'s',
-          brewer: 'Miller/Coors',
-          description: 'American Lager',
-          abv: '4.7%',
-          price: '3',
-          remaining: '65'
-        },
-        {
-          name: 'Prismatic',
-          brewer: 'Ninkasi',
-          description: 'Juicy IPA',
-          abv:  '5.9%',
-          price: '6',
-          remaining: '75'
-        },
-        {
-          name: 'Juicy Haze',
-          brewer: 'New Belgium',
-          description: 'India Pale Ale',
-          abv:  '7.5%',
-          price: '6',
-          remaining: '18'
-        },
-        {
-          name: '8 Hop',
-          brewer: 'New Belgium',
-          description: 'Pale Ale',
-          abv:  '5.5%',
-          price: '6',
-          remaining: '58'
-        }
-      ]
-    }
-  }
-  render(){
+export default function KegList(props) {
     return(
       <div className={styles.kegListWrapper}>
-        {this.state.masterKegList.map((keg, index) =>
+        {props.passedState.masterKegList.map((keg, index) =>
           <Keg
           name={keg.name}
           brewer={keg.brewer}
@@ -75,5 +18,4 @@ export default class KegList extends React.Component{
 
       </div>
     )
-  }
 }
